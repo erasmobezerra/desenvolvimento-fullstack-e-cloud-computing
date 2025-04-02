fece342015f587cfae142f57c6eab50aeb7030d7
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-04-02 10:34:56 -0300

    projeto da aula finalizado

--- modulo-01/02-javascript-e-aplicacoes-praticas/projetos/carrinho-de-compras/js/funcoes.js
+++ modulo-01/02-javascript-e-aplicacoes-praticas/projetos/carrinho-de-compras/js/funcoes.js
@@ -7,46 +7,35 @@ function alterarQuantidade(produto, acao) {
 
     if (qtd.innerHTML > 0 || acao == '+') {
         acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
-        total.innerHTML = valor.innerHTML * qtd.innerHTML
-        calcularSubtotal(valor, subtotal, acao)
+        const valorTotal = somenteNumeros(valor.innerHTML) * qtd.innerHTML
+        total.innerHTML = formatarValor(valorTotal)        
+        soma()
     } else {
         alert('Atenção! A quantidade não pode ser menor que zero')
     }
 
 }
 
-// A soma é resultado da adição ou subtração de cada valor 
-function calcularSubtotal(valor, subtotal, acao) {
-    let soma = Number(subtotal.innerHTML)
-    if (acao == '+') {
-        soma += Number(valor.innerHTML)
-        subtotal.innerHTML = soma.toFixed(2)
-    }
-    else {
-        soma -= Number(valor.innerHTML)
-        subtotal.innerHTML = soma.toFixed(2)
-    }
-}
-
 // Resolução do Professor: 
 // A soma é resultado da verificação dos valores dos produtos no momento e é feita a soma. 
-// function calcularSubtotal() {
-//     let soma = 0;
-//     for (let i = 0; i < 4; i++) {
-//         soma += Number(document.getElementById('total_' + i).innerHTML)
-//     }
-//     document.getElementById('subtotal').innerHTML = soma
-// }
+// Utiliza as funções de validação abaixo
+function soma() {
+    let soma = 0;
+    for (let i = 1; i < 4; i++) {
+        let numero = somenteNumeros(document.getElementById('total_' + i).innerHTML)
+        console.log(Number(numero))
+        soma += Number(numero)
+    }
+    document.getElementById('subtotal').innerHTML = formatarValor(soma)
+}
 
 // Mantém números e pontos decimais
 function somenteNumeros(n) {
-    return String(n).replace(/[^0-9.]/g, '');
+    return n.replace(/\D/g, '')
 }
 
-
-
 // Formatar um número como um valor monetário em reais (R$)
-function formatarValor(n){
+function formatarValor(n) {
     return 'R$ ' + n.toLocaleString('pt-BR')
 }
 
