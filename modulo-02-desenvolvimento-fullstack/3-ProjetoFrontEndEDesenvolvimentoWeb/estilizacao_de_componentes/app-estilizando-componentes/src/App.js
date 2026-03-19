fe6a2667253b39c8f9b98e560f2036c6cf3d35e0
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2026-03-19 13:15:13 -0300

    gerencfiamento avancado de estado

--- modulo-02-desenvolvimento-fullstack/3-ProjetoFrontEndEDesenvolvimentoWeb/estilizacao_de_componentes/app-estilizando-componentes/src/App.js
+++ modulo-02-desenvolvimento-fullstack/3-ProjetoFrontEndEDesenvolvimentoWeb/estilizacao_de_componentes/app-estilizando-componentes/src/App.js
@@ -3,6 +3,13 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import styled from "styled-components";
 
+// Styled Components
+// É uma biblioteca que possibilita a escrita de CSS diretamente nos componentes React, 
+// através da sintaxe de template literals do JavaScript. Isso permite a criação de estilos 
+// personalizados para cada componente, eliminando preocupações com conflitos de nomeação,
+// devido ao escopo local dos estilos.
+
+// Criando um componente estilizado usando styled-components
 const Button = styled.button`
   background-color: blue;
   color: white;
@@ -11,7 +18,8 @@ const Button = styled.button`
   padding: 8px 16px;
 `;
 
-
+// Criando um componente estilizado com base em props
+// O componente Button2 muda sua cor de fundo dependendo do valor da prop "primary"
 const Button2 = styled.button`
   background-color: ${props => props.primary ? 'blue' : 'gray'};
   color: white;
@@ -19,6 +27,7 @@ const Button2 = styled.button`
   border-radius: 4px;
   padding: 8px 16px;
 `;
+// ===========================================================
 
 // Criando um Componente React sem estilização
 function Header2({ className, children }) {
@@ -28,8 +37,10 @@ function Header2({ className, children }) {
     </h1>
   );
 }
+// ===========================================================
 
 // Criando um componente estilizado a partir de um componente React
+// O componente StyledHeader é criado a partir do componente Header, e recebe estilos adicionais
 const StyledHeader = styled(Header)`
   color: #BF4F74;
   font-weight: bold;
@@ -54,7 +65,6 @@ export default function App() {
       </Button2>
       <Header2>Unstyled Header</Header2> <br />
       <StyledHeader>Styled Header</StyledHeader>
-
     </div>
   );
 }
